856fdb3904cbdbef3fe88f3a163b39e8a96197c7
avigupta250 <avinashkumarnav52@gmail.com>
2024-12-25 18:59:48 +0530

    partner count section done

---
+++ src/components/PartnerCount.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { motion, useScroll, useTransform } from "motion/react";
+
+const PartnerCount = () => {
+  const { scrollYProgress } = useScroll();
+  const scroll = useScroll();
+  console.log(scroll);
+
+  console.log("scrollY", scrollYProgress);
+  // Transform the color based on the scroll progress
+  const textColor = useTransform(
+    scrollYProgress,
+    [0, 0.3, 0.5, 0.6, 0.7, 0.8, 0.9],
+    [
+      "#000000",
+      "#FF0000",
+      "#FF0000",
+      "#FF0000",
+      "#FF0000",
+      "#FF0000",
+      "#FF0000",
+    ]
+  ); // Black to Red
+  return (
+    <div className="h-screen flex justify-center items-center text-center bg-blac  w-full">
+      <div className="flex items-stretch justify-center itms-center  flex-col ">
+        <motion.span
+          style={{ color: textColor }}
+          className="text-[230px] leading-[230px]  font-bold "
+        >
+          75lacs+
+        </motion.span>
+        <h1 className="text-black font-bold text-[68px]">Forward-thinking partner</h1>
+      </div>
+    </div>
+  );
+};
+
+export default PartnerCount;
