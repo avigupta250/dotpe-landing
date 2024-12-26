9081015578b25b59113fd8a3b476d0c3f992203a
avigupta250 <avinashkumarnav52@gmail.com>
2024-12-26 13:04:19 +0530

    vite updated

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -5,12 +5,9 @@ import { IoIosArrowBack ,IoIosArrowForward} from "react-icons/io";
 
 // Import Swiper styles
 import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
 
-// import './styles.css';
 
-// import required modules
+
 import { Pagination, Navigation } from "swiper/modules";
 
 const Testimonial = () => {
