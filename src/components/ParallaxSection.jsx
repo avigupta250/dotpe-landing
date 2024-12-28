96ee90f30f29efa62f2a3313bfe9f249cd21cad4
avigupta250 <avinashkumarnav52@gmail.com>
2024-12-28 18:52:33 +0530

    parrallex mobile view fixed

--- src/components/ParallaxSection.jsx
+++ src/components/ParallaxSection.jsx
@@ -7,6 +7,7 @@ import React, { useEffect, useRef, useState } from "react";
 const ParallaxGallery = () => {
   const containerRef = useRef(null);
   const [scale, setScale] = useState(3); 
+  const [scale2,setScale2]=useState(1)
   const [opacity,setOpacity]=useState(0)
   const {scrollYProgress}=useScroll();
 //   const scroll=useScroll();
@@ -28,9 +29,11 @@ const ParallaxGallery = () => {
       
     
       const newScale = 3 - progress * 2;
+      const newScale2 = 2 - progress * 2;
       const newOpacity=1.5-progress*2 
     setOpacity(newOpacity)
       setScale(newScale);
+      setScale2(newScale2)
 
     //   console.log(opacity)
 
@@ -47,7 +50,7 @@ const ParallaxGallery = () => {
     <div ref={containerRef} className="min-h-[200vh] bg-gray-200">
       <div className="sticky top-0  overflow-hidden">
         <div
-          className="w-full  h-full transition-transform duration-75"
+          className="w-full hidden md:block  h-full transition-transform duration-75"
           style={{
             transform: `scale(${scale})`, 
             transition: "transform 0.1s linear",
@@ -59,14 +62,18 @@ const ParallaxGallery = () => {
             className="w-full  relative hidden md:block h-screen object-contain"
           />
           <motion.div 
-        //   initial={{opacity:0.1}}
-        //   whileInView={{opacity:1}}
-        //   exit={{opacity:0}}
-        //   transition={{duration:0.8,delay:0.5}}
+        
         style={{opacity:opacity}}
-          className={` absolute  transition-transform duration-150 items-center text-center top-[340px] left-[650px] z-1 text-black text-[16px] leading-[19px] font-bold`}
+          className={` absolute  transition-transform duration-150 items-center text-center top-[340px] left-[660px] z-1 text-black text-[16px] leading-[19px] font-bold`}
           >DotPe works for all types,<br/>size and formats of<br/> businesses</motion.div>
-          <img src="https://cdn.dotpe.in/dotpe-website-live/images/work-all-types-mobile.png"
+         
+        </div>
+        <div  className="w-full md:hidden  h-full transition-transform duration-75"
+          style={{
+            transform: `scale(${scale2})`, 
+            transition: "transform 0.1s linear",
+          }}>
+        <img src="https://cdn.dotpe.in/dotpe-website-live/images/work-all-types-mobile.png"
           className="w-full block md:hidden h-screen object-contain"
           />
         </div>
