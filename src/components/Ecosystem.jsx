0a0c794cb5ef726c960975cf9cf0106d18be421d
avigupta250 <avinashkumarnav52@gmail.com>
2024-12-28 14:42:43 +0530

    drowpdown down compltted

--- src/components/Ecosystem.jsx
+++ src/components/Ecosystem.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-import {motion} from "motion/react"
+import { motion } from "motion/react";
+import { IoArrowForward } from "react-icons/io5";
+import Dropdown from "../utils/Dropdown";
 const Ecosystem = () => {
   const services = [
     {
@@ -40,75 +42,156 @@ const Ecosystem = () => {
     },
   ];
 
+  const servicesMob = {
+    "Food & Beverages": [
+      {
+        // title: "Food & Beverage Industry",
+        desc: "Tools that connect the front of house with the back of house and integrate everything that you need to turn your restaurant into one dashboard",
+        images: {
+          image1:
+            "https://cdn.dotpe.in/dotpe-website-live/images/latest-fnb-image-1-240622.jpg",
+          image2:
+            "https://cdn.dotpe.in/dotpe-website-live/images/latest-fnb-image-2-240622.jpg",
+          image3:
+            "https://cdn.dotpe.in/dotpe-website-live/images/latest-fnb-image-3-240622.jpg",
+        },
+        detail:
+          "Tools for front of house, back of house, and everything in between.",
+      },
+    ],
+    Retail: [
+      {
+        // title: "Food & Beverage Industry",
+        // title: "Retail",
+        desc: "Solutions to help you sell online and offline seamlessly.",
+        images: {
+          image1:
+            "https://cdn.dotpe.in/dotpe-website-live/images/latest-retail-image-1-240622.jpg",
+          image2:
+            "https://cdn.dotpe.in/dotpe-website-live/images/latest-retail-image-2-240622.jpg",
+          image3:
+            "https://cdn.dotpe.in/dotpe-website-live/images/home/retail-img-3.jpg",
+        },
+      },
+    ],
+    "Services & Others": [
+      {
+        // title: "Services",
+        desc: "Efficient tools for health, repair, and other service professionals.",
+        images: {
+          image1:
+            "https://cdn.dotpe.in/dotpe-website-live/images/home/services-img-1.jpg",
+          image2:
+            "https://cdn.dotpe.in/dotpe-website-live/images/latest-services-image-2-240622.jpg",
+          image3:
+            "https://cdn.dotpe.in/dotpe-website-live/images/latest-services-image-3-240622.jpg",
+        },
+      },
+    ],
+  };
+
   const [serviceItem, setServiceItem] = useState(services[0]);
 
+  const [openSection, setOpenSection] = useState(null);
+  const handleToggle = (title) => {
+    setOpenSection(openSection === title ? null : title);
+  };
+
   return (
-    <section className="w-full  bg-black text-white py-20">
+    <section className="w-full  bg-black text-white pt-20  md:p md:py-20">
       <div className="flex justify-center mx-auto items-center">
         <div className="flex   flex-col">
           <motion.h1
-          initial={{y:-30,opacity:0.6}}
-          whileInView={{y:0,opacity:1}}
-          transition={{duration:0.3,delay:0.9}}
-           className="text-[50px] font-bold ml-[133px] max-w-[800px]">
+            initial={{ y: -30, opacity: 0.6 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.3, delay: 0.9 }}
+            className="md:text-[50px] font-bold md:ml-[133px] text-[30px]  px-6 md:px-0 max-w-[800px]"
+          >
             Ecosystem to sell everything through DotPe
           </motion.h1>
 
-          <div className="grid grid-cols-2  mt-10 gap-8">
-            {/* Services List */}
-            <div className="space-y-6 px-[120px] ">
-              {services.map((service, index) => (
-                <motion.div
-                initial={{x:-30,opacity:0}}
-                whileInView={{x:0,opacity:1}}
-                transition={{duration:0.3,delay:0}}
-                  key={index}
-                  onMouseEnter={() => {setServiceItem(service)
-                    console.log(service)}
-
-                  }
-                  onMouseLeave={()=>{setServiceItem(services[0]) 
-                    console.log(service)}}
-                  className={`${serviceItem.title===service.title?`text-white`:`text-[#898787]`} text-black  cursor-pointer p-4 transition-all duration-300`}
-              
-                >
-                  <h1 className="text-3xl font-semibold">{service.title}</h1>
-                  <p className="text-md leading-5 mt-4">{service.desc}</p>
-                  <button className={`${serviceItem.title===service.title?`bg-blue-600 text-white`:`bg-[#2B2B2B] text-gray-200`}  px-4 py-2 mt-3 rounded-md`}>
-                    Know More
-                  </button>
-                </motion.div>
-              ))}
-            </div>
-
-            {/* Image List */}
-            <div>
-            <div className="flex flex-col  space-y-4 transition-opacity duration-300">
-              <div className="overflow-hidden">
-                <img
-                  src={serviceItem.images["image1"]}
-                  alt="Service Image 1"
-                  className="w-full h-[370px] object-cover  rounded-3xl transition-transform duration-300 transform "
-                />
+          {/* mobile view */}
+          <div className="mt-10 md:hidden ">
+            {Object.entries(servicesMob).map(([title, items]) => (
+              <Dropdown
+                key={title}
+                title={title}
+                items={items}
+                isOpen={openSection === title}
+                onToggle={() => handleToggle(title)}
+              />
+            ))}
+          </div>
+          {/* Desktop bview */}
+          <div className="hidden md:block">
+            <div className="grid grid-cols-2  mt-10 gap-8">
+              {/* Services List */}
+              <div className="space-y-6 px-[120px] ">
+                {services.map((service, index) => (
+                  <motion.div
+                    initial={{ opacity: 0, x: -30 }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.5, delay: 0.8 }}
+                    key={index}
+                    onMouseEnter={() => {
+                      setServiceItem(service);
+                      console.log(service);
+                    }}
+                    onMouseLeave={() => {
+                      setServiceItem(services[0]);
+                      console.log(service);
+                    }}
+                    className={`${
+                      serviceItem.title === service.title
+                        ? `text-white`
+                        : `text-[#898787]`
+                    }   cursor-pointer p-4 transition-all duration-300`}
+                  >
+                    <h1 className="text-3xl flex items-center gap-4  font-semibold">
+                      {service.title} <IoArrowForward className="mt-2" />
+                    </h1>
+                    <p className="text-md leading-5 mt-4">{service.desc}</p>
+                    <button
+                      className={`${
+                        serviceItem.title === service.title
+                          ? `bg-blue-600 text-white`
+                          : `bg-[#2B2B2B] text-gray-200`
+                      }  px-3 py-1 mt-3 rounded-md`}
+                    >
+                      Know More
+                    </button>
+                  </motion.div>
+                ))}
               </div>
-              <div className="grid grid-cols-2 space-x-4">
-                <div className="overflow-hidden">
-                  <img
-                    src={serviceItem.images["image2"]}
-                    alt="Service Image 2"
-                    className="w-full object-cover rounded-3xl h-[250px] transition-transform duration-300 transform hover:scale-105"
-                  />
-                </div>
-                <div className="overflow-hidden">
-                  <img
-                    src={serviceItem.images["image3"]}
-                    alt="Service Image 3"
-                    className="w-full object-cover h-[250px] h-auo rounded-3xl transition-transform duration-300 transform hover:scale-105"
-                  />
+
+              {/* Image List */}
+              <div>
+                <div className="flex flex-col  space-y-4 transition-opacity duration-300">
+                  <div className="overflow-hidden">
+                    <img
+                      src={serviceItem.images["image1"]}
+                      alt="Service Image 1"
+                      className="w-full h-[370px] object-cover  rounded-3xl transition-transform duration-300 transform "
+                    />
+                  </div>
+                  <div className="grid grid-cols-2 space-x-4">
+                    <div className="overflow-hidden">
+                      <img
+                        src={serviceItem.images["image2"]}
+                        alt="Service Image 2"
+                        className="w-full object-cover rounded-3xl h-[250px] transition-transform duration-300 transform hover:scale-105"
+                      />
+                    </div>
+                    <div className="overflow-hidden">
+                      <img
+                        src={serviceItem.images["image3"]}
+                        alt="Service Image 3"
+                        className="w-full object-cover h-[250px] h-auo rounded-3xl transition-transform duration-300 transform hover:scale-105"
+                      />
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-
             </div>
           </div>
         </div>
---
+++ src/utils/Dropdown.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import { AnimatePresence, motion } from "motion/react";
+import { FaBars, FaMinus } from "react-icons/fa6";
+import { FaPlus } from "react-icons/fa";
+const Dropdown = ({items,key,title,isOpen,onToggle}) => {
+    // console.log(items[0].images)
+  return (
+    <div className='border-b-[1px] border-slate-700'>
+    <button
+      onClick={onToggle}
+      className="flex mt-3 justify-between mb-3  items-center w-full"
+    >
+      <span className="px-6 font-gilroy font-bold text-lg ">{title}</span>
+      <span className="px-3">{!isOpen ? <FaPlus /> : <FaMinus />}</span>
+    </button>
+
+    <AnimatePresence>
+      {isOpen && (
+        <motion.div
+          initial={{ height: 0, opacity: 0 }}
+          animate={{ height: "auto", opacity: 1 }}
+          exit={{ height: 0, opacity: 0 }}
+          transition={{ duration: 0.3 }}
+        //   className=" bg-[#F8F8F8] b-1"
+          className=" bg-black b-1 "
+        >
+          <ul className="space-y-4 py-4 ">
+            {items.map((item, index) => (
+              <li key={index} className="px-5">
+              {item.title&&<h4 className="font-gilroy font-bold ">{item.title}</h4>}  
+                {item.desc && (
+                  <p className="font-gilroy text-gray-400 text-[14px]  ">{item.desc}</p>
+                )}
+              </li>
+            ))}
+          </ul>
+
+        {!items[0].title&&  <button className='bg-blue-500 ml-5 text-white px-3 py-1 mt-3 rounded-md '>Know more</button>}
+           {/* Image List */}
+           <div>
+            <div className="flex px-5 mt-12 flex-col bg-black space-y-4 mb-8 transition-opacity duration-300">
+              <div className="overflow-hidden">
+                <img
+                  src={items[0].images["image1"]}
+                  alt="Service Image 1"
+                  className="w-full  object-cover  rounded-xl transition-transform duration-300 transform "
+                />
+              </div>
+              <div className="grid grid-cols-2 space-x-4">
+                <div className="overflow-hidden">
+                  <img
+                     src={items[0].images["image2"]}
+                    alt="Service Image 2"
+                    className="w-full object-cover h-full rounded-xl transition-transform duration-300 transform "
+                  />
+                </div>
+                <div className="overflow-hidden">
+                  <img
+                    src={items[0].images["image3"]}
+                    alt="Service Image 3"
+                    className="w-full object-cover  rounded-xl transition-transform duration-300 transform "
+                  />
+                </div>
+              </div>
+            </div>
+
+            </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  </div>
+  )
+}
+
+export default Dropdown
