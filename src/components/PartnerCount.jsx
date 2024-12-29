fa04f9c1b0e07af97ae31ddba833a15df8560da4
avigupta250 <avinashkumarnav52@gmail.com>
2024-12-29 12:09:49 +0530

    range

--- src/components/PartnerCount.jsx
+++ src/components/PartnerCount.jsx
@@ -14,7 +14,7 @@ console.log(roundedScroll)
     return () => unsubscribe(); 
   }, [scrollYProgress]);
 
-  const range1= [0.70, 0.71,0.72,0.74,0.75,0.76,0.77,0.78,0.79,0.80]
+  const range1= [0.75,0.76,0.77,0.78,0.79,0.80,0.83,0.85,0.88,0.92]
   const range2= [0.65,0.68,0.70,0.73,0.76,0.79,0.82]
 
   const colors1=[
